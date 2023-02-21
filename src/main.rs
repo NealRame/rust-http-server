f09df504f061f8d84a2ac25465b1e452f639aa42
Julien Graziano <julien@graziano.fr>
2023-02-21 23:07:09 +0100

    feat: parse request URI

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
-const FALLBACK_CONTENT: &str = "\
+const INDEX_FALLBACK: &str = "\
 <!DOCTYPE html>\
 <html lang='en'>\
     <head>\
@@ -12,6 +12,19 @@ const FALLBACK_CONTENT: &str = "\
     <body>\
         <h1>Hello!</h1>\
         <p>Hi from Rust</p>\
+    </body>\
+</html>";
+
+const ERROR_404_FALLBACK: &str = "\
+<!DOCTYPE html>\
+<html lang='en'>\
+    <head>\
+        <meta charset='utf-8'>\
+        <title>404 Not found</title>\
+    </head>\
+    <body>\
+        <h1>404</h1>\
+        <p>Page not found</p>\
     </body>\
 </html>";
 
@@ -30,19 +43,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let but_reader = BufReader::new(&stream);
-    let req: Vec<_> = but_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    let status = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html").unwrap_or(String::from(FALLBACK_CONTENT));
+    let request_line = but_reader.lines().next().unwrap().unwrap();
+    let (status, file, fallback) =
+        if request_line == "GET / HTTP/1.1" {
+            (
+                "HTTP/1.1 200 OK",
+                "index.html",
+                INDEX_FALLBACK,
+            )
+        } else {
+            (
+                "HTTP/1.1 400 NOT FOUND",
+                "404.html",
+                ERROR_404_FALLBACK,
+            )
+        };
+    let content = fs::read_to_string(file).unwrap_or(String::from(fallback));
     let header_content_length = format!("Content-Length: {}", content.len());
-
     let res = format!("{status}\r\n{header_content_length}\r\n\r\n{content}");
-
-    // println!("Request: {:#?}", http_request);
-
     stream.write_all(res.as_bytes()).unwrap();
 }
